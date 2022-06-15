5235b1ff94cd05f326fa44c0c20066bf1c0b2517
ildysilva <ildeberto34@gmail.com>
2022-06-15 16:47:24 +0100

    add toolbars

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,35 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SideNavigatorComponent } from './components/side-navigator/side-navigator.component';
+import { ToolBarComponent } from './components/tool-bar/tool-bar.component';
+import { BottomBarComponent } from './components/bottom-bar/bottom-bar.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {  MatSidenavModule } from "@angular/material/sidenav";
+import { MatListModule } from "@angular/material/list";
+import { MatToolbarModule} from "@angular/material/toolbar";
+import { MatIconModule  } from "@angular/material/icon";
+import { DashboardComponent } from './view/dashboard/dashboard.component';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatButtonModule } from '@angular/material/button';
+import { LayoutModule } from '@angular/cdk/layout';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SideNavigatorComponent,
+    ToolBarComponent,
+    BottomBarComponent,
+    DashboardComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatSidenavModule,MatListModule,MatToolbarModule, MatIconModule, MatGridListModule, MatCardModule, MatMenuModule, MatButtonModule, LayoutModule 
   ],
   providers: [],
   bootstrap: [AppComponent]
